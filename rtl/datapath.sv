b49c9e797a3a6a663d822f012c9d47e4f447773d
danial_rube <dnlnorozi@gmail.com>
2026-08-01 01:03:45 +0330

    Fix Data Path syntax

--- rtl/datapath.sv
+++ rtl/datapath.sv
@@ -1,5 +1,5 @@
 module datapath #(
-    parameter FILE_NAME = "default.hex"
+    parameter string FILE_NAME = "default.hex"
 )
 (
     input logic clk, rst,
@@ -7,7 +7,7 @@ module datapath #(
     input logic IRWrite,
     input logic sav_en, swap_en,
     input logic PCWrite,
-    // input logic PCSrc,
+    input logic PCSrc,
     input logic [1:0] ALUOp,
     input logic [3:0] srcType,
 
@@ -34,7 +34,7 @@ module datapath #(
     //==================================
     logic [23:0] instr;
     logic [23:0] fetched_instr;
-    logic [23:0] imm_extend;
+    logic signed [23:0] imm_extend;
 
     assign imm_extend = {{12{instr[11]}}, instr[11:0]}; // Extended immediate 
     assign up_instr = instr[23:12];  // send to contorler
@@ -48,7 +48,7 @@ module datapath #(
         .instr(fetched_instr)
     );
 
-    always_ff @(clk) begin
+    always_ff @(posedge clk) begin
         if(IRWrite)
             instr <= fetched_instr;
     end
@@ -57,7 +57,8 @@ module datapath #(
     //        Register File 
     //==================================
 
-    logic [23:0] acc_value;
+    logic signed [23:0] acc_value;
+    logic signed [23:0] alu_out;
     RegisterFile RF(
         .rst(rst),
         .clk(clk),
@@ -76,17 +77,16 @@ module datapath #(
     //==================================
     //             ALU 
     //==================================
-    logic [23:0] src_mux_out;
+    logic signed [23:0] src_mux_out;
     always_comb begin
         case (srcType)
-            4'h0: src_mux_out = acc_value;      // ACC
-            4'h1: src_mux_out = 24'sd0;             // NIL
-            4'h8: src_mux_out = imm_extend;         // IMM
+            4'b00: src_mux_out = acc_value;      // ACC
+            4'b01: src_mux_out = 24'sd0;             // NIL
+            4'b10: src_mux_out = imm_extend;         // IMM
             default: src_mux_out = 24'sd0;     // LEFT, RIGHT, UP, DOWN, ANY, LAST هنوز تعریف نشده در رابط پورت ها
         endcase
     end
 
-    logic [23:0] alu_out;
     ALU u_ALU(
         .ALUOp(ALUOp),
         .srcA(acc_value),
